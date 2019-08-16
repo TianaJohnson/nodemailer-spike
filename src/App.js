73f97381fbd4e84cd826da0bb26d399fd8ee1d42
Tiana Johnson <tiana.marie013@gmail.com>
2019-08-16 09:55:13 -0500

    a rebuild to figure out where I went wrong

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, Form, FormGroup, Input, Label} from 'reactstrap';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form>
+          <FormGroup>
+            <Label>Name: </Label>
+            <Input
+            type="text"
+            name="name"
+            // onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Email: </Label>
+            <Input
+            type="text"
+            name="email"
+            // onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>message: </Label>
+            <Input
+            type="text"
+            name="message"
+            // onChange={this.handleChange}
+            />
+          </FormGroup>
+          <Button>Submit!</Button>
+
+        </Form>
+        
+        
       </header>
     </div>
   );
